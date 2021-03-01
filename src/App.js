319390a7fe7a78d777db977fd709acbe4db3b2f2
FetterPiskvik <petterfisk@gmail.com>
2021-03-01 04:53:19 +0100

    Summary (required)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import React from 'react';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
     <Router>
-      <div className="App">
+      <Navbar />
         <Switch>
           <Route path = "/" exact component = {Home} />
           <Route path = "/login" component = {Login} />
           <Route path = "/register" component = {Register} />
         </Switch>
-      </div>
     </Router>
+    </>
   );
 }
 
