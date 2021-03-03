08d7decdd5d5940696c44ad3e78f8db99410a31f
Ajdin Bajrovic <ajdinbajrovic@hotmail.no>
2021-03-03 17:55:29 +0100

    Fikset plasseringen til bildet for forsiden

--- src/pages/Frontpage/Components/HeroSection.js
+++ src/pages/Frontpage/Components/HeroSection.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../../App.css';
 import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import '../CSS/HeroSection.css';
 import reise from '../../../assets/reise.svg';
 
 
-function HeroSection( {
-    lightBg, lightText, lightTextDesc, headline, description, img, alt, imgStart
+function HeroSection({
+    lightBg, lightText, lightTextDesc, headline, description, imgStart
 }) {
     return (
         <>
@@ -18,18 +19,27 @@ function HeroSection( {
                     style={{display: 'flex', flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'}}
                     >
                         <div className="column">
-                            <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
-                            <p className={lightTextDesc ? 'home_hero-subtitle' : 'home_hero-subtitle dark'}>{description}</p>
-                            <Button className='button' variant="contained" color="default">Sett i gang reisen</Button>                   
+                            <div className="home_hero-text-wrapper">
+                                <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+                                <p
+                                    className={lightTextDesc ? 'home_hero-subtitle' : 'home_hero-subtitle dark'}
+                                >
+                                    {description}
+                                </p>
+                                <Link to="/Registrer" style={{ textDecoration: 'none' }}>
+                                    <Button className='button' variant="contained" color="default">Sett i gang reisen</Button>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                <div className="column"></div>
-                    <div className="home_hero-img-wrapper">
-                        <img src={reise} alt='Student reise' className="img_destination" imgStart=''/>
+                        <div className="column">
+                            <div className="home_hero-img-wrapper">
+                                <img src={reise} alt='Student reise' className='home_hero-img' imgstart=''  />
+                            </div> 
+                        </div>
                     </div>
                 </div>
             </div>
-        </>
+        </>       
     );
 }
 
