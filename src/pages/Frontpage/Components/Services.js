ec64ffe5541440e2d76100ed6fda5eff514dbaaa
Ajdin Bajrovic <ajdinbajrovic@hotmail.no>
2021-03-04 21:07:41 +0100

    Satt i gang Services seksjon for forsiden

---
+++ src/pages/Frontpage/Components/Services.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div>
+            <div className="services_section"></div>
+                <div className="services_wrapper"></div>
+                    <h1 className="services_heading">Overskrift</h1>
+                        <div className="services_container">
+                            <div className="services_container-card">
+                                <div className="icon">
+
+                                </div>
+                                <h3>Kortets overskrift</h3>
+                                <p>Innhold i kortet</p>
+                            </div>                            
+                        </div>
+
+        </div>
+    )
+}
+
+export default Services
