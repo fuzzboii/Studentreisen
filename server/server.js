29e8f53330a580cb87078fbd41878ecb75eb45fd
RK-USN <226996@usn.no>
2021-03-06 06:10:29 +0100

    Studentmarka til Studentreisen, oops

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 
-// Temporary header to allow localhost:3000 as an origin, likely will be changed to "https://studentmarka.no:80" or similar
+// Temporary header to allow localhost:3000 as an origin, likely will be changed to "https://studentreisen.no:80" or similar
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
