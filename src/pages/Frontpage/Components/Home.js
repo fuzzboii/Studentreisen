bd70525c30c00450eb72c3c6e7cdbb3277945599
Ajdin Bajrovic <ajdinbajrovic@hotmail.no>
2021-03-06 16:56:39 +0100

    Enkelte endringer i Services

--- src/pages/Frontpage/Components/Home.js
+++ src/pages/Frontpage/Components/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../../../App.css'
 import HeroSection from './HeroSection'
 import {homeObjOne} from './Data'
+import Services from './Services'
 
 function Home() {
     return (
         <>
-            <HeroSection {...homeObjOne} /> 
+            <HeroSection {...homeObjOne} />
+            <Services /> 
             
         </>
     )
--- src/pages/Frontpage/Components/Services.js
+++ src/pages/Frontpage/Components/Services.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import { FaChalkboardTeacher } from "react-icons/fa";
+import '../CSS/Services.css';
+
+
 
 function Services() {
     return (
         <div>
-            <div className="services_section"></div>
-                <div className="services_wrapper"></div>
+            <div className="services_section">
+                <div className="services_wrapper">
                     <h1 className="services_heading">Overskrift</h1>
                         <div className="services_container">
                             <div className="services_container-card">
                                 <div className="icon">
-
+                                    <FaChalkboardTeacher size="2em" />
                                 </div>
                                 <h3>Kortets overskrift</h3>
                                 <p>Innhold i kortet</p>
                             </div>                            
                         </div>
-
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Services
