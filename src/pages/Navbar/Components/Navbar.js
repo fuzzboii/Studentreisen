b7a4189d369b59fbf7ee9a6d5a9c11cc660f504e
FetterPiskvik <petterfisk@gmail.com>
2021-03-08 21:57:52 +0100

    Navbar
    Gått over til mer MUI.

--- src/pages/Navbar/Components/Navbar.js
+++ src/pages/Navbar/Components/Navbar.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../CSS/Navbar.css';
 import favicon from '../../../assets/usn.png';
-import { Button } from '../../../global/Components/Button';
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -24,69 +27,107 @@ function Navbar() {
     }, []); 
 
     window.addEventListener('resize', showButton);
+
+    const [anchorKurs, setAnchorKurs] = React.useState(null);
+    const handleClickAnchorKurs = event => {
+    setAnchorKurs(event.currentTarget);
+    };
+    const handleCloseKurs = () => {
+    setAnchorKurs(null);
+    };
+
+    const [anchorSeminarer, setAnchorSeminarer] = React.useState(null);
+    const handleClickAnchorSeminarer = event => {
+    setAnchorSeminarer(event.currentTarget);
+    };
+    const handleCloseSeminarer = () => {
+    setAnchorSeminarer(null);
+    };
+
+    const useStyles = makeStyles({
+      navbtn: {
+        color: '#fff',
+        fontSize: '1.5rem',
+        padding: '0.5rem 1rem',
+        display: 'flex',
+        height: '100%',
+      },
+
+      loggbtn: {
+        color: '#fff',
+        fontSize: '1rem',
+        padding: '0.5rem 1rem',
+        display: 'flex',
+        height: '100%'
+      }
+    });
     
+    const classes = useStyles();
+
     return (
         <>
           <nav className='navbar'>
             <div className='navbar-container'>
-              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <Link 
+                to="/" 
+                className="navbar-logo" 
+                onClick={closeMobileMenu}>
                 <img className="navbar-logo-png" src={favicon} alt="USN" />
               </Link>
               <div className='menu-icon' onClick={handleClick}>
                 <i className={click? 'fas fa-times' : 'fas fa-bars'} />
               </div>
               
-              <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <li>
-                  <Link
-                    to='/'
+              <div className={click ? 'nav-menu active' : 'nav-menu'}>
+                  <Button
                     className='nav-links-mobile'
-                    onClick={closeMobileMenu}
                   >
                     Logg ut
-                  </Link>
-                </li>
+                  </Button>
 
-                <li className='nav-item'>
-                  <Link 
-                    to='/' 
-                    className='nav-links' 
-                    onClick={closeMobileMenu}
-                  >
-                    Hjem
-                  </Link>
-                </li>
+                <Button 
+                  className={classes.navbtn} 
+                  aria-controls="simple-menu" 
+                  aria-haspopup="true" 
+                  onClick={handleClickAnchorKurs}>
+                Kurs
+                </Button>
+                <Menu
+                id="kurs-menu" 
+                anchorEl={anchorKurs}
+                keepMounted
+                open={Boolean(anchorKurs)}
+                onClose={handleCloseKurs}
+                >
+                <MenuItem onClick={handleCloseKurs}>Nyheter</MenuItem>
+                <MenuItem onClick={handleCloseKurs}>Favoritter</MenuItem>
+                <MenuItem onClick={handleCloseKurs}>Arkiv</MenuItem>
+                </Menu>
 
-                <li className='nav-item'>
-                  <Link
-                    to='/'
-                    className='nav-links'
-                    onClick={closeMobileMenu}
-                  >
-                    Kurs
-                  </Link>
-                </li>
-
-                <li className='nav-item'>
-                  <Link
-                    to='/'
-                    className='nav-links'
-                    onClick={closeMobileMenu}
-                  >
-                    Seminarer
-                  </Link>
-                </li>
+                <Button 
+                  className={classes.navbtn} 
+                  aria-controls="simple-menu" 
+                  aria-haspopup="true" 
+                  onClick={handleClickAnchorSeminarer}
+                >
+                Seminarer
+                </Button>
+                <Menu
+                id="seminarer-menu"
+                anchorEl={anchorSeminarer}
+                keepMounted
+                open={Boolean(anchorSeminarer)}
+                onClose={handleCloseSeminarer}>
+                <MenuItem onClick={handleCloseSeminarer}>Nyheter</MenuItem>
+                <MenuItem onClick={handleCloseSeminarer}>Favoritter</MenuItem>
+                <MenuItem onClick={handleCloseSeminarer}>Arkiv</MenuItem>
+                </Menu>
     
-                <li>
-                  <Link
-                    to='/'
-                    className='nav-links'
-                    onClick={closeMobileMenu}
-                  >
-                    CV
-                  </Link>
-                </li>
-              </ul>
+                <Button
+                  className={classes.navbtn}>
+                  CV
+                </Button>
+              </div>
               
               <Link
                 to="/"
@@ -95,11 +136,11 @@ function Navbar() {
               >
                 <i className="far fa-user" />
               </Link>
-              {button && <Button className="logout-btn" buttonStyle='btn--outline'>LOGG UT</Button> }
+              {button && <Button className={classes.loggbtn} >LOGG UT</Button> }
             </div> 
           </nav>
         </>
       );
     }
 
-export default Navbar
+export default Navbar;
