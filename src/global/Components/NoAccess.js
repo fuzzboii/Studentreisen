c7e22f1574c0828ffdf612c391890779dc67d17b
RK-USN <226996@usn.no>
2021-03-10 13:17:23 +0100

    Fjernet Section fra NoAccess

--- src/global/Components/NoAccess.js
+++ src/global/Components/NoAccess.js
@@ -3,10 +3,8 @@ import '../CSS/NoAccess.css';
 function NoAccess() {
     return (
         <main id="main_no_access">
-            <section id="section_no_access">
-                <h1>Du kan ikke se denne siden</h1>
-                <p>Om du mener dette ikke stemmer kan du kontakte support på <a href="mailto:support@studentreisen.no">support@studentreisen.no</a></p>
-            </section>
+            <h1>Du kan ikke se denne siden</h1>
+            <p>Om du mener dette ikke stemmer kan du kontakte support på <a href="mailto:support@studentreisen.no">support@studentreisen.no</a></p>
         </main>
     );
 }
