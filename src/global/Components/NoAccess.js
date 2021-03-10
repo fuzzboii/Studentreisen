0bd24b254fbf8d7fadb8d0fa8f1205dfbab9c9ef
RK-USN <226996@usn.no>
2021-03-10 13:05:44 +0100

    Indentering i NoAccess

--- src/global/Components/NoAccess.js
+++ src/global/Components/NoAccess.js
@@ -1,12 +1,12 @@
 import '../CSS/NoAccess.css';
 
 function NoAccess() {
-  return (
-    <section id="section_no_access">
-        <h1>Du kan ikke se denne siden</h1>
-        <p>Om du mener dette ikke stemmer kan du kontakte support på <a href="mailto:support@studentreisen.no">support@studentreisen.no</a></p>
-    </section>
-  );
+    return (
+        <section id="section_no_access">
+            <h1>Du kan ikke se denne siden</h1>
+            <p>Om du mener dette ikke stemmer kan du kontakte support på <a href="mailto:support@studentreisen.no">support@studentreisen.no</a></p>
+        </section>
+    );
 }
 
 
