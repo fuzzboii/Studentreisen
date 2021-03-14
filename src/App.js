8f20d8b0993a89e31abfa4c9ec43ad83d1f9f1be
fuzzboii <54532695+fuzzboii@users.noreply.github.com>
2021-03-14 17:31:02 +0100

    Mappen 'Course' etablert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import Login from './pages/Login/Components/Login';
 import Register from './pages/Register/Components/Register';
 import Reset from './pages/Reset/Components/Reset';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Frontpage/Components/Home';
 import Navbar from './pages/Navbar/Components/Navbar';
 import Footer from './global/Components/Footer';
+import Course from './pages/Coursepage/Components/Course';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Switch>
           <Route path = "/" exact component = {Home} />
           <Route path = "/login" component = {Login} />
+          <Route path = "/course" component = {Course} />
           <Route path = "/register" component = {Register} />
           <Route path = "/reset" component = {Reset} />
         </Switch>
