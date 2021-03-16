f9d1733b7bf55c324eb36e58d69dd6942a117f6b
RK-USN <226996@usn.no>
2021-03-16 13:57:41 +0100

    Første versjon av Tools

--- src/pages/Tools/Components/Tools.js
+++ src/pages/Tools/Components/Tools.js
@@ -1,17 +1,171 @@
 // React-spesifikt
+import React, {useState, useMemo} from 'react'
 import { Component } from "react";
 
+// 3rd-party Packages
+import ReactSwipe from 'react-swipe';
+
 // Studentreisen-assets og komponenter
+import UserOverview from './UserOverview';
 import '../CSS/Tools.css';
+import Loader from '../../../global/Components/Loader';
+import NoAccess from '../../../global/Components/NoAccess';
+import CookieService from '../../../global/Services/CookieService';
+import AuthService from '../../../global/Services/AuthService';
 
 class Tools extends Component {
     constructor(props) {
-        super(props)
-    };
+        super(props);
+        
+        this.state = {
+            loading : true, authenticated : false,
+            windowWidth : 0,
+            activeTool : 0,
+        }
+    }
+
+    handleWindowResize() {
+        this.setState({
+            windowWidth: window.innerWidth
+        })
+    }
+
+    componentDidMount() {
+        this.handleWindowResize();
+        window.addEventListener('resize', this.handleWindowResize.bind(this));
+
+        // Henter authtoken-cookie
+        const token = CookieService.get("authtoken");
+
+        if(token !== undefined) {
+        // Om token eksisterer sjekker vi mot serveren om brukeren har en gyldig token
+        AuthService.isAuthenticated(token).then(res => {
+            if(!res) {
+            // Sletter authtoken om token eksisterer lokalt men ikke er gyldig på server
+            CookieService.remove("authtoken");
+            }
+            this.setState({
+            authenticated : res,
+            loading: false
+            });
+        });
+        } else {
+        this.setState({
+            authenticated : false,
+            loading: false
+        });
+        }
+    }
 
     render() {
-        <></>
+        const {loading, authenticated} = this.state;
+
+        if(loading) {
+            // Om vi er i loading fasen (Før mottatt data fra API) vises det et Loading ikon
+            return(
+                <section id="loading">
+                    <Loader />
+                </section>
+            );
+        }
+        
+        if(!loading && authenticated) {
+            if(this.state.windowWidth < 960) {
+                return (
+                    <Swipe />
+                )
+            } else {
+                if(this.state.activeTool == 0) {
+                    return (
+                        <UserOverview />
+                    )
+                } else if(this.state.activeTool == 1) {
+                    return (
+                        <main>
+                            <div>
+                                <h1>Kursoversikt</h1>
+                            </div>
+                        </main>
+                    )
+                } else if(this.state.activeTool == 2) {
+                    return (
+                        <main>
+                            <div>
+                                <h1>Seminaroversikt</h1>
+                            </div>
+                        </main>
+                    )
+                }
+            }
+        } else {
+            return (
+                // Ugyldig eller ikke-eksisterende token 
+                <NoAccess />
+            );
+        }
     }
 }
+
+
+const Swipe = () => {
+    const [position, setPosition] = useState(0);
+  
+    const swipeOptions = useMemo(() => ({
+        continuous: false,
+        transitionEnd(e) {
+            setPosition(e)
+        }
+    }), []);
+
+    const changeActiveIndicator = () => {
+        switch(position) {
+            case 0: 
+                return (
+                    <section id="tools_indicator">
+                        <button className="tools_indicator_btn_active" variant="outlined" />
+                        <button className="tools_indicator_btn" variant="outlined" />
+                        <button className="tools_indicator_btn" variant="outlined" />
+                    </section>
+                )
+            case 1: 
+                return (
+                    <section id="tools_indicator">
+                        <button className="tools_indicator_btn" variant="outlined" />
+                        <button className="tools_indicator_btn_active" variant="outlined" />
+                        <button className="tools_indicator_btn" variant="outlined" />
+                    </section>
+                )
+            case 2: 
+                return (
+                    <section id="tools_indicator">
+                        <button className="tools_indicator_btn" variant="outlined" />
+                        <button className="tools_indicator_btn" variant="outlined" />
+                        <button className="tools_indicator_btn_active" variant="outlined" />
+                    </section>
+                )
+        }
+    }
+  
+    return (
+        <main>
+            {changeActiveIndicator()}
+            <ReactSwipe id="swipe_tools" swipeOptions={swipeOptions}>
+                <div className="div_tools">
+                    <UserOverview />
+                </div>
+                <div className="div_tools">
+                    <section>
+                        <h1>Kursoversikt</h1>
+                    </section>
+                </div>
+                <div className="div_tools">
+                    <section>
+                        <h1>Seminaroversikt</h1>
+                    </section>
+                </div>
+            </ReactSwipe>
+        </main>
+    );
+};
 
 export default Tools;
--- src/pages/Tools/Components/UserOverview.js
+++ src/pages/Tools/Components/UserOverview.js
@@ -1,54 +1,243 @@
 // React-spesifikt
-import { Component } from "react";
+import React, { Component } from "react";
 
 // 3rd-party Packages
+import { Button, FormControl, InputLabel, Input, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Checkbox, FormControlLabel } from '@material-ui/core';
+import { Search as SearchIcon, PersonOutlineOutlined as PersonOutlineIcon, MailOutline as MailOutlineIcon } from '@material-ui/icons';
+import { useTable, usePagination } from 'react-table'
 
 // Studentreisen-assets og komponenter
 import '../CSS/UserOverview.css';
+import axios from "axios";
+import CookieService from '../../../global/Services/CookieService';
 
-class UserOverview extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            userOverview : [
-                {"id" : 1, "fnavn" : "Test" , "enavn" : "Testersen"}
-            ]
-        }
-    }
 
-    updateUserOverview() {
-        this.setState({
-            // Hent slide vi foreløpig er på
-            swipeStartSlide: this.state.swipeStartSlide,
-        })
+const GetAllData = () => {
+    const token = {
+        token: CookieService.get("authtoken")
     }
+    // Axios POST request
+    axios
+        // Henter API URL fra .env og utfører en POST request med dataen fra objektet over
+        // Axios serialiserer objektet til JSON selv
+        .post(process.env.REACT_APP_APIURL + "/tools/getAllUserData", token)
+        // Utføres ved mottatt resultat
+        .then(res => {
+            allData = res.data.results;
+            allDataFetched = true;
+        }).catch(err => {
 
-    render() {
-        return (
-                <section>
-                    <h1>Brukeroversikt</h1>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Fornavn</th>
-                                <th>Etternavn</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.userOverview.map(user => (
-                                <tr key={user.id}>
-                                    <td>{user.id}</td>
-                                    <td>{user.fnavn}</td>
-                                    <td>{user.enavn}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </section>
-        )
-    }
+        }).finally(() => {
+
+        });
 }
+
+
+let allData = GetAllData();
+let allDataFetched = false;
+
+function UserOverview() {
+    const [data, setData] = React.useState([]);
+    const [fetching, setFetching] = React.useState(false);
+    const [pageAmount, setPageAmount] = React.useState(0);
+    const fetchIDReference = React.useRef(0);
+
+    const columns = React.useMemo(() => 
+        [
+            {
+                Header: "ID",
+                columns: [
+                    {
+                        Header: "#",
+                        accessor: "brukerid"
+                    }
+                ]
+            },
+            {
+                Header: "Brukerinfo",
+                columns: [
+                    {
+                        Header: "Fornavn",
+                        accessor: "fnavn"
+                    },
+                    {
+                        Header: "Etternavn",
+                        accessor: "enavn"
+                    }
+                ]
+            },
+            {
+                Header: "Ekstra info",
+                columns: [
+                    {
+                        Header: "Telefon",
+                        accessor: "telefon"
+                    },
+                    {
+                        Header: "E-post",
+                        accessor: "email"
+                    }
+                ]
+            }
+        ], []
+    );
+
+    const fetchData = React.useCallback(({ pageSize, pageIndex }) => {
+        const fetchID = ++fetchIDReference.current;
+
+        setFetching(true);
+        
+        const continueFetchingData = () => {
+            if(fetchID === fetchIDReference.current ) {
+                const startRow = pageSize * pageIndex;
+                const endRow = startRow + pageSize;
+    
+                setData(allData.slice(startRow, endRow));
+    
+                setPageAmount(Math.ceil(allData.length / pageSize));
+    
+                setFetching(false);
+            }
+        }
+
+        const checkAllData = () => {
+            if(allDataFetched) {
+                continueFetchingData();
+            } else { 
+                setTimeout(checkAllData, 50);
+            }
+        }
+
+        checkAllData();
+    }, []);
+
+    return (
+        <TableOverview
+            columns={columns}
+            data={data}
+            fetchData={fetchData}
+            fetching={fetching}
+            pageAmount={pageAmount}
+        />
+    )
+}
+
+// 
+function TableOverview({ columns, data, fetchData, fetching, pageAmount: controlledPageCount }) {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable({columns, data, initialState: { pageIndex: 0 }, manualPagination: true, pageCount: controlledPageCount},
+        usePagination);
+    
+    React.useEffect(() => {
+        fetchData({ pageIndex, pageSize })
+    }, [fetchData, pageIndex, pageSize]);
+
+    return (
+        <>
+        <section id="section_useroverview">
+            <h1>Brukeroversikt</h1>
+            <table {...getTableProps()} id="table_useroverview">
+                <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        <th {...column.getHeaderProps()}>
+                        {column.render('Header')}
+                        <span>
+                            {column.isSorted
+                            ? column.isSortedDesc
+                                ? ' 🔽'
+                                : ' 🔼'
+                            : ''}
+                        </span>
+                        </th>
+                    ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                {page.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        })}
+                    </tr>
+                    )
+                })}
+                <tr>
+                    {fetching ? (
+                        <td colSpan="10000">Laster...</td>
+                    ) : (
+                        <td colSpan="10000">
+                            Viser {page.length} av ~{controlledPageCount * pageSize} resultater
+                        </td>
+                    )}
+                </tr>
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                {'>>'}
+                </button>{' '}
+                <span>
+                Page{' '}
+                <strong>
+                    {pageIndex + 1} of {pageOptions.length}
+                </strong>{' '}
+                </span>
+                <span>
+                | Go to page:{' '}
+                <input
+                    type="number"
+                    defaultValue={pageIndex + 1}
+                    onChange={e => {
+                    const page = e.target.value ? Number(e.target.value) - 1 : 0
+                    gotoPage(page)
+                    }}
+                    style={{ width: '100px' }}
+                />
+                </span>{' '}
+                <select
+                value={pageSize}
+                onChange={e => {
+                    setPageSize(Number(e.target.value))
+                }}
+                >
+                {[10, 20, 30, 40, 50].map(pageSize => (
+                    <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                    </option>
+                ))}
+                </select>
+            </div>
+        </section>
+        </>
+    );
+};
 
 export default UserOverview;
