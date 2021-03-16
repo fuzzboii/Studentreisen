bd6ad80811d73548a3a2a206f38a5e92827de3df
RK-USN <226996@usn.no>
2021-03-16 15:56:00 +0100

    Lagt til kontroll mot tilgangsnivå i getAllUserData, foreløpig har kun Admin tilgang

--- server/routes/tools.js
+++ server/routes/tools.js
@@ -16,19 +16,43 @@ router.post('/getAllUserData', async (req, res) => {
             }
 
             if(results[0] !== undefined) {
-                let getDataQuery = "SELECT brukerid, fnavn, enavn, telefon, email FROM bruker";
-                let getDataQueryFormat = mysql.format(getDataQuery);
+                // authtoken er gyldig, sjekker om bruker har tilstrekkelige rettigheter for å kunne se dette innholdet
+                checkQuery = "SELECT brukerid, niva FROM bruker WHERE brukerid = ?";
+                checkQueryFormat = mysql.format(checkQuery, [results[0].gjelderfor]);
 
-                connection.query(getDataQueryFormat, (error, results) => {
+                connection.query(checkQueryFormat, (error, results) => {
                     if (error) {
-                        console.log("En feil oppstod ved henting av all brukerdata: " + error.errno + ", " + error.sqlMessage)
-                        return res.json({ "status" : "error", "message" : "En intern feil oppstod, vennligst forsøk igjen senere" });
-                    }   
-                    
-                    if(results[0] !== undefined) {
-                        return res.json({results});
+                        console.log("En feil oppstod ved henting av bruker nivå i BrukerOversikt: " + error.errno + ", " + error.sqlMessage);
+                        return res.json({ "status" : "error", "message" : "Ingen tilgang, om feilen fortsetter, forsøk å logg ut og inn igjen" });
                     }
-                });
+
+                    if(results[0] !== undefined) {
+                        // Kun Administrator skal kunne se oversikten
+                        if(results[0].niva.toString() === process.env.ACCESS_ADMINISTRATOR) {
+                            let getDataQuery = "SELECT brukerid, fnavn, enavn, telefon, email FROM bruker";
+                            let getDataQueryFormat = mysql.format(getDataQuery);
+    
+                            connection.query(getDataQueryFormat, (error, results) => {
+                                if (error) {
+                                    console.log("En feil oppstod ved henting av all brukerdata: " + error.errno + ", " + error.sqlMessage)
+                                    return res.json({ "status" : "error", "message" : "En intern feil oppstod, vennligst forsøk igjen senere" });
+                                }   
+                                
+                                if(results[0] !== undefined) {
+                                    return res.json({results});
+                                }
+                            });
+                        } else {
+                            // Bruker har ikke tilgang, loggfører
+                            console.log("En innlogget bruker uten riktige tilganger har forsøkt å se brukeroversikten, brukerens ID: " + results[0].brukerid)
+                            return res.json({ "status" : "error", "message" : "Ingen tilgang, om feilen fortsetter, forsøk å logg ut og inn igjen" });
+                        }
+                    } else {
+                        return res.json({ "status" : "error", "message" : "Ingen tilgang, om feilen fortsetter, forsøk å logg ut og inn igjen" });
+                    }
+                })
+
+
             } else {
                 return res.status(403).send();
             }
