46888818f84d43e8cf771315fb3a5fb6dfd34b43
fuzzboii <54532695+fuzzboii@users.noreply.github.com>
2021-03-18 11:42:24 +0100

    Kurs filer

---
+++ src/pages/Coursepage/Components/Overview.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+
+class Overview extends Component {
+    constructor(props) {
+        super(props);
+    };
+    
+    render() {
+        return(
+        <div>
+            <h1>Kurs</h1>
+        </div>
+        );
+    }    
+}   
+
+
+export default Overview; 
