1c09aba10e920f19badf262afbddc7914d4ee640
RK-USN <226996@usn.no>
2021-03-24 09:16:20 +0100

    Om telefon har en verdi, test om denne er ok

--- src/global/Services/ValidationService.js
+++ src/global/Services/ValidationService.js
@@ -1,9 +1,17 @@
 class ValidationService {
     validateUser(user) {
         if(user.fnavn !== undefined && user.enavn !== undefined && user.niva !== undefined && user.email !== undefined) {
-            if(user.niva === "1" || user.niva === "2" || user.niva === "3" || user.niva === "4") {
+            if(user.niva === "1" || user.niva === 1 || user.niva === "2" || user.niva === 2 || user.niva === "3" || user.niva === 3 || user.niva === "4" || user.niva === 4) {
                 if(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(user.email)) {
-                    return true;
+                    if(user.telefon !== undefined) {
+                        if(/^(00|\+){0,2}?[0-9]{0,2}[0-9]{8}$/.test(user.telefon)) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return true;
+                    }
                 } else {
                     return false;
                 }
