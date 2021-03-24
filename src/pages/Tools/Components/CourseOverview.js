cc740cce30394e78c805c13d217d4ab3179b2532
RK-USN <226996@usn.no>
2021-03-24 11:36:25 +0100

    Opprettet kursoversikt

---
+++ src/pages/Tools/Components/CourseOverview.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+// 3rd-party Packages
+import MaterialTable from "material-table";
+import axios from "axios";
+
+// Studentreisen-assets og komponenter
+import CookieService from '../../../global/Services/CookieService';
+
+function CourseOverview(props) {
+    let [kurs, setKurs] = React.useState([]);
+    
+    const token = {
+        token: CookieService.get("authtoken")
+    }
+
+    if(token !== undefined && Object.getOwnPropertyNames(kurs).length == 1) {
+        axios
+            // Henter API URL fra .env og utfører en POST request med dataen fra objektet over
+            // Axios serialiserer objektet til JSON selv
+            .post(process.env.REACT_APP_APIURL + "/tools/getAllCourseData", token)
+            // Utføres ved mottatt resultat
+            .then(res => {
+                if(res.data.results) {
+                    setKurs(res.data.results);
+                }
+            });
+    }
+    
+    const kolonner = [
+        { title: "Emnekode", field: "emnekode" },
+        { title: "Navn", field: "navn" },
+        { title: "Semester", field: "semester" },
+        { title: "Studiepoeng", field: "studiepoeng", type: "numeric" }
+    ];
+    
+    const lokalisering = {
+        pagination: {
+            labelDisplayedRows: "{from}-{to} av {count}",
+            labelRowsSelect: "kurs",
+            labelRowsPerPage: "Kurs per side:",
+            firstAriaLabel: "Første side", 
+            firstTooltip: "Første side", 
+            previousAriaLabel: "Forrige side",
+            previousTooltip: "Forrige side",
+            nextAriaLabel: "Neste side",
+            nextTooltip: "Neste side",
+            lastAriaLabel: "Siste side",
+            lastTooltip: "Siste side",
+        },
+        header: {
+            actions: "Valg"
+        },
+        body: {
+            deleteTooltip: "Slett kurs",
+            emptyDataSourceMessage: "Ingen kurs å vise",
+            filterRow: {
+                filterTooltip: "Filter"
+            },
+            editRow: {
+                deleteText: "Er du sikker på at du ønsker å slette dette kurset?",
+                cancelTooltip: "Avbryt",
+                saveTooltip: "Godkjenn"
+            }
+        },
+        toolbar: {
+            searchTooltip: "Søk",
+            searchPlaceholder: "Søk"
+        }
+    }
+
+    return (
+        <section id="section_overview">
+            <MaterialTable columns={kolonner} data={kurs} localization={lokalisering} title="Kursoversikt" actions={[
+                    {
+                        icon: 'edit',
+                        tooltip: 'Rediger kurs',
+                        onClick: (e, kursData) => {
+                            console.log("Gå til redigering for " + kursData.emnekode + "\nHusk at all data er med her (kursData.beskrivelse, kursData.lenke osv), trenger ikke hente på nytt");
+                        }
+                    },
+                    {
+                        icon: 'add',
+                        tooltip: 'Nytt kurs',
+                        isFreeAction: true,
+                        onClick: () => {
+                            console.log("Gå til oppretting av nytt kurs");
+                        }
+                    }
+                ]}
+            />
+        </section>
+    );
+}
+
+export default CourseOverview;
