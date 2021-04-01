a570279b7079e74d6af0222df340f4083ad989e2
fuzzboii <54532695+fuzzboii@users.noreply.github.com>
2021-04-01 12:10:15 +0200

    Lagt til spesifik kursmodul

---
+++ src/pages/Coursepage/Components/ModuleDetail.js
@@ -0,0 +1,92 @@
+import { useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import { MyCardContent } from '../Styles/apistyles';
+
+import axios from 'axios';
+import Box from '@material-ui/core/Box';
+
+import SchoolIcon from '@material-ui/icons/School';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+// Studentreisen-assets og komponenter
+import '../Styles/courseStyles.css';
+import Loader from '../../../global/Components/Loader';
+import NoAccess from '../../../global/Components/NoAccess';
+
+
+const ModuleDetail = (props) => {
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    let { modulkode } = useParams();
+    const [modules, setModules] = useState([]);
+
+    const fetchData = async () => {
+                    
+        const res = await axios.get(process.env.REACT_APP_APIURL + "/course/module");
+        console.log(res.data);
+        setModules(res.data);
+
+    };
+
+    return(
+        <>
+        {props.loading &&
+            // Om vi er i loading fasen (Før mottatt data fra API) vises det et Loading ikon
+            <section id="loading">
+                <Loader />
+            </section>
+        }
+        {!props.loading && props.auth &&
+                <div className="course-detail">
+                {modules.map(mod => { if(modulkode === mod.modulkode)           
+                        return <Box className="box-detail" boxShadow={3}>
+                                    <MyCardContent>
+                                        <Breadcrumbs aria-label="breadcrumb">
+                                            <Link color="inherit" href="/course">
+                                                Kurs
+                                            </Link>
+                                            <Typography color="textPrimary">Spesifikk</Typography>
+                                        </Breadcrumbs>
+                                        <div className="courseHeader">
+                                            <h1 className="overskriftKurs">{mod.navn}</h1>
+                                            <div className="kursinfo-tekst">
+                                                <p>{mod.modulkode}</p>
+                                                <div className="iconBox">
+                                                    <p className="undervisningsspråk">{mod.campus}</p>
+                                                </div>
+                                                <div className="iconBox">
+                                                    
+                                                    <p>{mod.studietype}</p>
+                                                </div>
+                                                <div className="iconBox">
+                                                    <SchoolIcon className="language-icon2" fontSize="inherit"/>
+                                                    <p>{mod.studiepoeng}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="courseBody">
+                                            <h2>Sammendrag</h2>|
+                                            <p>{mod.beskrivelse}</p>
+                                        </div>
+                                        <div className="buttonWrap">
+                                            <Button className="courseButton" variant="outlined" color="primary" href={mod.lenke}>Gå til kursets hjemmeside</Button>
+                                        </div>
+                                    </MyCardContent>
+                                </Box>         
+                })}
+                </div>
+        
+        }{!props.loading && !props.auth &&
+                // Ugyldig eller ikke-eksisterende token 
+                <NoAccess />
+        }
+        </>
+    );
+};
+
+export default ModuleDetail;
