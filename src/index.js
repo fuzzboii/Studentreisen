7d046e384cfc8781313aeb79fbb87acfe89f3adf
Coolstream <coolstream@mail.com>
2021-04-20 06:15:17 +0200

    Opprettet Snackbar for meldinger

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import {BrowserRouter as Router, Switch, Route, useLocation} from 'react-router-dom';
+import {SnackbarProvider} from 'notistack';
+import Slide from '@material-ui/core/Slide';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router >
-      <App />
+      <SnackbarProvider 
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        TransitionComponent={Slide}
+      >
+        <App />
+      </SnackbarProvider>
     </Router >
   </React.StrictMode>,
   document.getElementById('root')
