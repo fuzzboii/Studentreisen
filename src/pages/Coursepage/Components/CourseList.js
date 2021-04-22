0190c77a28b2c1288c576fad45b23cf529577ba1
fuzzboii <54532695+fuzzboii@users.noreply.github.com>
2021-04-23 00:49:03 +0200

    unik key på courselist

--- src/pages/Coursepage/Components/CourseList.js
+++ src/pages/Coursepage/Components/CourseList.js
@@ -5,8 +5,8 @@ const CourseList = ({courses}) => {
 
     return (
         <div className="course-list">
-            {courses.map(course => (               
-                <Link key={course.emnekode} className='link' to={`/course/emnekode=${course.emnekode}`}>
+            {courses.map((course,index) => (               
+                <Link key={index} className='link' to={`/course/emnekode=${course.emnekode}`}>
                     <Course emnekode={course.emnekode} navn={course.navn} beskrivelse={course.beskrivelse} språk={course.språk} semester={course.semester} studiepoeng={course.studiepoeng} lenke={course.lenke}/>
                 </Link>     
             ))}
